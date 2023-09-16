b5255cf776c6084a1b3c047646eee15cd72945ac
a1ekseevkiri11 <alekseevkirill30092004@mail.ru>
2023-09-17 02:04:21 +0500

    сверстал калькулятор

---
+++ Creating a calculator/Creating a calculator.js
@@ -0,0 +1,24 @@
+// class Scrin{
+//     constructor(value, block){
+//         this._value = value;
+//         this._block = block;
+//     }
+
+//     write(num){
+//     }
+
+//     setToZero(){
+
+//     }
+// }
+
+// class Calculator{
+//     constructor(){
+//         this._scrinBlock;
+//     }
+// }
+
+
+// window.addEventListener('DOMContentLoaded', () => {
+
+// });
