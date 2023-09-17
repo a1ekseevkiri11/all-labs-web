4d16dd6fe092765b3f74febc4d0383b806771a1c
a1ekseevkiri11 <alekseevkirill30092004@mail.ru>
2023-09-18 01:09:18 +0500

    поправил верстку, поработал с кодом

--- Creating a calculator/Creating a calculator.js
+++ Creating a calculator/Creating a calculator.js
@@ -4,19 +4,66 @@
 //         this._block = block;
 //     }
 
-//     write(num){
-//     }
-
 //     setToZero(){
+//         this.val = 0;
+//         this.write()
+//     }
+    
+//     write(){
 
 //     }
 // }
+
+
+// class Button{
+//     constructor(value, block){
+//         this._value = value;
+//         this._block = block;
+//     }
+// }
+
+// class Keyboard{
+//     constructor(){
+//         initInput()
+//     }
+    
+//     initInput(){
+//         document.getElementById("C/CE").addEventListener("click", (e) =>{});
+//         document.getElementById("OFF").addEventListener("click", (e) =>{});
+//         document.getElementById("sqrt").addEventListener("click", (e) =>{});
+//         document.getElementById("%").addEventListener("click", (e) =>{});
+//         document.getElementById("MRC").addEventListener("click", (e) =>{});
+//         document.getElementById("M-").addEventListener("click", (e) =>{});
+//         document.getElementById("M+").addEventListener("click", (e) =>{});
+//         document.getElementById("/").addEventListener("click", (e) =>{});
+//         document.getElementById("7").addEventListener("click", (e) =>{});
+//         document.getElementById("8").addEventListener("click", (e) =>{});
+//         document.getElementById("9").addEventListener("click", (e) =>{});
+//         document.getElementById("*").addEventListener("click", (e) =>{});
+//         document.getElementById("4").addEventListener("click", (e) =>{});
+//         document.getElementById("5").addEventListener("click", (e) =>{});
+//         document.getElementById("6").addEventListener("click", (e) =>{});
+//         document.getElementById("-").addEventListener("click", (e) =>{});
+//         document.getElementById("1").addEventListener("click", (e) =>{});
+//         document.getElementById("2").addEventListener("click", (e) =>{});
+//         document.getElementById("3").addEventListener("click", (e) =>{});
+//         document.getElementById("+").addEventListener("click", (e) =>{});
+//         document.getElementById("0").addEventListener("click", (e) =>{});
+//         document.getElementById(".").addEventListener("click", (e) =>{});
+//         document.getElementById("=").addEventListener("click", (e) =>{});
+//     }
+// }
+
 
 // class Calculator{
 //     constructor(){
 //         this._scrinBlock;
 //     }
+
+    
 // }
+
+
 
 
 // window.addEventListener('DOMContentLoaded', () => {
