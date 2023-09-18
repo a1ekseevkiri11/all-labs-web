4eb83cbbf185eb05db477c54afea7331af4cc5b1
a1ekseevkiri11 <alekseevkirill30092004@mail.ru>
2023-09-18 21:18:16 +0500

    дописал калькулятор

--- Creating a calculator/Creating a calculator.js
+++ Creating a calculator/Creating a calculator.js
@@ -1,71 +1,84 @@
-// class Scrin{
-//     constructor(value, block){
-//         this._value = value;
-//         this._block = block;
-//     }
+const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const sign = ["+", "-", "/", "*"];
 
-//     setToZero(){
-//         this.val = 0;
-//         this.write()
-//     }
+class Calculator{
+    constructor(){
+        this.scrin_block = document.getElementById("screen");
+        this.ac();
+        this.initInput();
+    }
     
-//     write(){
-
-//     }
-// }
-
-
-// class Button{
-//     constructor(value, block){
-//         this._value = value;
-//         this._block = block;
-//     }
-// }
-
-// class Keyboard{
-//     constructor(){
-//         initInput()
-//     }
+    initInput(){
+        let buttons = document.querySelectorAll("button");
+        
+        for(let i = 0; i < buttons.length; i++){
+            buttons[i].addEventListener("click", (e) => {
+                let key = e.target.textContent;
+                if(digits.includes(key)){
+                    if(this._screen_val.length === 11){
+                        return;
+                    }
+                    if(this._screen_val === "0"){
+                        this.cutLastCharScrinBlock();
+                    }
+                    this._screen_val += key;
+                    this.scrin_block.textContent = this._screen_val;
+                    return;
+                }
+                if(sign.includes(key)){
+                    if(sign.includes(this._screen_val.at(-1))){
+                        this.cutLastCharScrinBlock();
+                    }
+                    this._screen_val += key;
+                    this.write();
+                    return;
+                }
+                if(key === "="){
+                    this.calc();
+                    return;
+                }
+                if(key === "AC"){
+                    this.ac();
+                    return;
+                }
+            });
+        }
+    }
     
-//     initInput(){
-//         document.getElementById("C/CE").addEventListener("click", (e) =>{});
-//         document.getElementById("OFF").addEventListener("click", (e) =>{});
-//         document.getElementById("sqrt").addEventListener("click", (e) =>{});
-//         document.getElementById("%").addEventListener("click", (e) =>{});
-//         document.getElementById("MRC").addEventListener("click", (e) =>{});
-//         document.getElementById("M-").addEventListener("click", (e) =>{});
-//         document.getElementById("M+").addEventListener("click", (e) =>{});
-//         document.getElementById("/").addEventListener("click", (e) =>{});
-//         document.getElementById("7").addEventListener("click", (e) =>{});
-//         document.getElementById("8").addEventListener("click", (e) =>{});
-//         document.getElementById("9").addEventListener("click", (e) =>{});
-//         document.getElementById("*").addEventListener("click", (e) =>{});
-//         document.getElementById("4").addEventListener("click", (e) =>{});
-//         document.getElementById("5").addEventListener("click", (e) =>{});
-//         document.getElementById("6").addEventListener("click", (e) =>{});
-//         document.getElementById("-").addEventListener("click", (e) =>{});
-//         document.getElementById("1").addEventListener("click", (e) =>{});
-//         document.getElementById("2").addEventListener("click", (e) =>{});
-//         document.getElementById("3").addEventListener("click", (e) =>{});
-//         document.getElementById("+").addEventListener("click", (e) =>{});
-//         document.getElementById("0").addEventListener("click", (e) =>{});
-//         document.getElementById(".").addEventListener("click", (e) =>{});
-//         document.getElementById("=").addEventListener("click", (e) =>{});
-//     }
-// }
-
-
-// class Calculator{
-//     constructor(){
-//         this._scrinBlock;
-//     }
-
+    ac(){
+        this._screen_val = "";
+        this.write();
+    }
     
-// }
+    calc(){
+        try {
+            if(sign.includes(this._screen_val.at(-1))){
+                this.cutLastCharScrinBlock();
+            }
+            let answer = eval(this._screen_val);
+            if(answer >= 1000000000){
+                answer("Число вылезло за пределы экрана, вывел только то что влезло!")
+                this._screen_val = String(answer);
+            }
+            this._screen_val = String(answer);
+        } catch (error) {
+            alert("ТЫ СДЕЛАЛ ЧТО-ТО НЕ ТАК\nНЕ ДЕЛАЙ ТАК БОЛЬШЕ!");
+            this.ac();
+        }
+        this.write();
+    }
+
+    cutLastCharScrinBlock(){
+        let new_string =  this._screen_val.slice(0, -1);
+        this._screen_val = new_string;
+    }
+
+    write(){
+        this.scrin_block.textContent = this._screen_val;
+    }
+}
 
 
-
-
-// window.addEventListener('DOMContentLoaded', () => {
-
-// });
+window.addEventListener('DOMContentLoaded', () => {
+    const APP = new Calculator();
+});
