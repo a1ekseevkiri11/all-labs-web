30136ab8e338c30fd37b7890bc3122c7ec19b378
a1ekseevkiri11 <alekseevkirill30092004@mail.ru>
2023-09-20 23:18:50 +0500

    написал змейку

--- Working with DOM/Working with DOM.js
+++ Working with DOM/Working with DOM.js
@@ -144,158 +144,245 @@ function getCoords(elem) {
 
 //10
 
-let scoreBlock;
-let score = 0;
-
-const config = {
-	step: 0,
-	maxStep: 10,
-	sizeCell: 16,
-	sizeBerry: 16 / 4
-}
-
-const snake = {
-	x: 160,
-	y: 160,
-	dx: config.sizeCell,
-	dy: 0,
-	tails: [],
-	maxTails: 3
-}
-
-let berry = {
-	x: 0,
-	y: 0
-} 
-
-
-let canvas = document.querySelector("#game-canvas");
-let context = canvas.getContext("2d");
-scoreBlock = document.querySelector(".game-score .score-count");
-drawScore();
-
-function gameLoop() {
-
-	requestAnimationFrame( gameLoop );
-	if ( ++config.step < config.maxStep) {
-		return;
-	}
-	config.step = 0;
-
-	context.clearRect(0, 0, canvas.width, canvas.height);
-
-	drawBerry();
-	drawSnake();
-}
-requestAnimationFrame( gameLoop );
-
-function drawSnake() {
-	snake.x += snake.dx;
-	snake.y += snake.dy;
-
-	collisionBorder();
-
-	// todo бордер
-	snake.tails.unshift( { x: snake.x, y: snake.y } );
-
-	if ( snake.tails.length > snake.maxTails ) {
-		snake.tails.pop();
-	}
-
-	snake.tails.forEach( function(el, index){
-		if (index == 0) {
-			context.fillStyle = "#FA0556";
-		} else {
-			context.fillStyle = "#A00034";
-		}
-		context.fillRect( el.x, el.y, config.sizeCell, config.sizeCell );
-
-		if ( el.x === berry.x && el.y === berry.y ) {
-			snake.maxTails++;
-			incScore();
-			randomPositionBerry();
-		}
-
-		for( let i = index + 1; i < snake.tails.length; i++ ) {
-
-			if ( el.x == snake.tails[i].x && el.y == snake.tails[i].y ) {
-				refreshGame();
-			}
-
-		}
-
-	} );
-}
-
-function collisionBorder() {
-	if (snake.x < 0) {
-		snake.x = canvas.width - config.sizeCell;
-	} else if ( snake.x >= canvas.width ) {
-		snake.x = 0;
-	}
-
-	if (snake.y < 0) {
-		snake.y = canvas.height - config.sizeCell;
-	} else if ( snake.y >= canvas.height ) {
-		snake.y = 0;
-	}
-}
-function refreshGame() {
-	score = 0;
-	drawScore();
-
-	snake.x = 160;
-	snake.y = 160;
-	snake.tails = [];
-	snake.maxTails = 3;
-	snake.dx = config.sizeCell;
-	snake.dy = 0;
-
-	randomPositionBerry();
-}
-
-function drawBerry() {
-	context.beginPath();
-	context.fillStyle = "#A00034";
-	context.arc( berry.x + (config.sizeCell / 2 ), berry.y + (config.sizeCell / 2 ), config.sizeBerry, 0, 2 * Math.PI );
-	context.fill();
-}
-
-function randomPositionBerry() {
-	berry.x = getRandomInt( 0, canvas.width / config.sizeCell ) * config.sizeCell;
-	berry.y = getRandomInt( 0, canvas.height / config.sizeCell ) * config.sizeCell;
-}
-
-function incScore() {
-	score++;
-	drawScore();
-}
-
-function drawScore() {
-	scoreBlock.innerHTML = score;
-}
+//support function
 
 function getRandomInt(min, max) {
 	return Math.floor( Math.random() * (max - min) + min );
 }
 
-document.addEventListener("keydown", function (e) {
-	if ( e.code == "KeyW" ) {
-		snake.dy = -config.sizeCell;
-		snake.dx = 0;
-	} else if ( e.code == "KeyA" ) {
-		snake.dx = -config.sizeCell;
-		snake.dy = 0;
-	} else if ( e.code == "KeyS" ) {
-		snake.dy = config.sizeCell;
-		snake.dx = 0;
-	} else if ( e.code == "KeyD" ) {
-		snake.dx = config.sizeCell;
-		snake.dy = 0;
+class Config {
+	constructor() {
+		this.step = 0;
+		this.maxStep = 20;
+		this.sizeCell = 16;
+		this.sizeBerry = this.sizeCell / 4;
 	}
-});
+}
 
-// window.addEventListener('DOMContentLoaded', () => {
-//     const APP = new Game();
-// });
+class Canvas {
+    constructor( container ) {
+        this.element = document.createElement( "canvas" );
+        this.context = this.element.getContext( "2d" );
+        this.element.width = 320;
+        this.element.height = 400;
+        container.appendChild( this.element );
+    }
+}
+
+class Game {
+    constructor( container ) {
+        this.canvas_ = new Canvas( container );
+        this.snake_ = new Snake();
+        this.berry_ = new Berry( this.canvas_ );
+        this.score_ = new Score( ".game-score .score-count", 0 );
+        new GameLoop( this.update.bind(this), this.draw.bind(this) );
+    }
+
+    update() {
+        this.snake_.update( this.berry_, this.score_, this.canvas_ );
+    }
+
+    draw() {
+        this.canvas_.context.clearRect( 0, 0, this.canvas_.element.width, this.canvas_.element.height );
+        this.snake_.draw( this.canvas_.context );
+        this.berry_.draw( this.canvas_.context );
+
+    }
+
+}
+
+class Score {
+    constructor( scoreBlock, score = 0 ) {
+
+        this.scoreBlock = document.querySelector( scoreBlock );
+        this.score = score;
+
+        this.draw();
+
+    }
+
+    incScore() {
+        this.score++;
+        this.draw();
+    }
+
+    setToZero() {
+        this.score = 0;
+        this.draw();
+    }
+
+    draw() {
+        this.scoreBlock.innerHTML = this.score;
+    }
+    
+}
+
+class Snake {
+	
+	constructor(){
+
+		this.config = new Config();
+		this.x = 160;
+		this.y = 160;
+		this.dx = this.config.sizeCell;
+		this.dy = 0;
+		this.tails = [];
+		this.maxTails = 3;
+
+		this.control();
+
+	}
+
+	update( berry, score, canvas ) {
+		this.x += this.dx;
+		this.y += this.dy;
+	
+		if (this.x < 0) {
+			this.x = canvas.element.width - this.config.sizeCell;
+		} else if ( this.x >= canvas.element.width ) {
+			this.x = 0;
+		}
+	
+		if (this.y < 0) {
+			this.y = canvas.element.height - this.config.sizeCell;
+		} else if ( this.y >= canvas.element.height ) {
+			this.y = 0;
+		}
+	
+		this.tails.unshift( { x: this.x, y: this.y } );
+	
+		if ( this.tails.length > this.maxTails ) {
+			this.tails.pop();
+		}
+	
+		this.tails.forEach( (el, index) => {
+	
+			if ( el.x === berry.x && el.y === berry.y ) {
+				this.maxTails++;
+				score.incScore();
+				berry.randomPosition();
+			}
+	
+			for( let i = index + 1; i < this.tails.length; i++ ) {
+	
+				if ( el.x == this.tails[i].x && el.y == this.tails[i].y ) {
+					this.death();
+					score.setToZero();
+					berry.randomPosition();
+				}
+	
+			}
+	
+		} );
+
+	}
+
+	draw(context) {
+
+		this.tails.forEach( (el, index) => {
+			if (index == 0) {
+				context.fillStyle = "#FA0556";
+			} else {
+				context.fillStyle = "#A00034";
+			}
+			context.fillRect( el.x, el.y, this.config.sizeCell, this.config.sizeCell );
+		} );
+
+	}
+
+	death() {
+
+		this.x = 160;
+		this.y = 160;
+		this.dx = this.config.sizeCell;
+		this.dy = 0;
+		this.tails = [];
+		this.maxTails = 3;
+
+	}
+
+	control() {
+		
+		document.addEventListener("keydown",  (e) => {
+			if ( e.code == "KeyW" && this.dy !== this.config.sizeCell) {
+				this.dy = -this.config.sizeCell;
+				this.dx = 0;
+			} else if ( e.code == "KeyA" && this.dx !== this.config.sizeCell) {
+				this.dx = -this.config.sizeCell;
+				this.dy = 0;
+			} else if ( e.code == "KeyS" && this.dy !== -this.config.sizeCell) {
+				this.dy = this.config.sizeCell;
+				this.dx = 0;
+			} else if ( e.code == "KeyD" && this.dx !== -this.config.sizeCell ) {
+				this.dx = this.config.sizeCell;
+				this.dy = 0;
+			}
+		});
+
+	}
+
+}
+
+class GameLoop {
+
+    constructor( update, draw ) {
+
+        this.update = update;
+        this.draw = draw;
+
+        this.config = new Config();
+
+        this.animate = this.animate.bind(this);
+        this.animate();
+
+    }
+
+    animate() {
+
+        requestAnimationFrame( this.animate );
+        if ( ++this.config.step < this.config.maxStep) {
+            return;
+        }
+        this.config.step = 0;
+
+        this.update();
+        this.draw();
+
+    }
+
+}
+
+class Berry {
+
+    constructor( canvas ) {
+
+        this.x = 0;
+        this.y = 0;
+        this.canvas = canvas;
+
+        this.config = new Config();
+        this.randomPosition();
+
+    }
+
+    draw(context) {
+
+        context.beginPath();
+        context.fillStyle = "#A00034";
+        context.arc( this.x + (this.config.sizeCell / 2 ), this.y + (this.config.sizeCell / 2 ), this.config.sizeBerry, 0, 2 * Math.PI );
+        context.fill();
+
+    }
+
+    randomPosition() {
+        this.x = getRandomInt( 0, this.canvas.element.width / this.config.sizeCell ) * this.config.sizeCell;
+        this.y = getRandomInt( 0, this.canvas.element.height / this.config.sizeCell ) * this.config.sizeCell;
+    }
+
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const APP = new Game(document.querySelector(".canvas-wrapper"));
+});
 
